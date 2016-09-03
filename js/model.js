8874878d20b3ffd5929e1f1122107cac52aac614
Safia25 <safia.benaouda@gmail.com>
2016-09-03 15:15:13 -0700

    Update Map

---
+++ js/model.js
@@ -0,0 +1,9 @@
+        // These are the restaurants that will be shown to the user.
+        var locations = [
+          {title: 'Gjusta', location: {lat: 33.995193, lng: -118.474435}},
+          {title: 'Rose Cafe', location: {lat: 33.996239, lng: -118.477100}},
+          {title: 'Wabi Sabi', location: {lat: 33.989674, lng: -118.463012}},
+          {title: 'Rustic Canyon Wine Bar and Seasonal Kitchen', location: {lat: 34.025081, lng: -118.491336}},
+          {title: 'The Butchers Daughter', location: {lat: 33.991679, lng: -118.469192}},
+          {title: 'Salt Air', location: {lat: 33.989600, lng: -118.463696}}
+        ];

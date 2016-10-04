ca08b26fcbfd168d9792ae33fa85f7c280c52309
Safia25 <safia.benaouda@gmail.com>
2016-10-03 17:18:57 -0700

    Add files via upload

---
+++ js/model.js
@@ -0,0 +1,9 @@
+        // These are the restaurants that will be shown to the user.
+        var restaurants = [
+          {title: 'Gjusta', yelp: 'gjusta-los-angeles-3', location: {lat: 33.995193, lng: -118.474435}},
+          {title: 'Rose Cafe', yelp: 'rose-cafe-and-restaurant-venice', location: {lat: 33.996239, lng: -118.477100}},
+          {title: 'Wabi Sabi', yelp: 'wabi-sabi-sushi-and-solana-venice', location: {lat: 33.989674, lng: -118.463012}},
+          {title: 'Rustic Canyon Wine Bar and Seasonal Kitchen', yelp: 'rustic-canyon-santa-monica', location: {lat: 34.025081, lng: -118.491336}},
+          {title: 'The Butchers Daughter', yelp: 'the-butchers-daughter-venice-2', location: {lat: 33.991679, lng: -118.469192}},
+          {title: 'Salt Air', yelp: 'salt-air-venice', location: {lat: 33.989600, lng: -118.463696}}
+        ];
